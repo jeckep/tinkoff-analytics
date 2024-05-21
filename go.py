9a2dd460a848fa5dcdc7fcd917b2e85bd616807d
Evgenii Poluektov <evgeniy.v.poluektov@gmail.com>
2024-05-21 23:44:23 +0500

    print all cash in/cach outs for all account for a user

--- go.py
+++ go.py
@@ -8,48 +8,55 @@ load_dotenv()
 
 token = os.environ["TINKOFF_TOKEN"]
 
+OPERATION_TYPE_INPUT = OperationType(1)
+OPERATION_TYPE_OUTPUT = OperationType(9)
+
+
+def get_acc_ids():
+    with Client(token) as client:
+        accounts = client.users.get_accounts().accounts
+        return [acc.id for acc in accounts]
 
 def print_deposites():
     with Client(token) as client:
-        accounts = client.users.get_accounts()
-        account_id = accounts.accounts[0].id
-
-        def get_request(cursor=""):
-            return GetOperationsByCursorRequest(
-                # from_=datetime(2022, 1, 1),
-                account_id=account_id,
-                operation_types=[OperationType(1)],  # only PayIn operations
-                cursor=cursor
-            )
-
-        deposites = []
-        operations = client.operations.get_operations_by_cursor(get_request())
-        for op in operations.items:
-            deposites.append({'currency': op.payment.currency,
-                              'amount': op.payment.units, 'date': op.date.strftime('%Y-%m-%d')})
-
-        while operations.has_next:
-            request = get_request(cursor=operations.next_cursor)
-            operations = client.operations.get_operations_by_cursor(request)
+        op_dtos = []
+        accounts = get_acc_ids()
+        for account_id in accounts:
+            def get_request(cursor=""):
+                return GetOperationsByCursorRequest(
+                    account_id=account_id,
+                    operation_types=[OPERATION_TYPE_INPUT, OPERATION_TYPE_OUTPUT],
+                    cursor=cursor
+                )
+            operations = client.operations.get_operations_by_cursor(get_request())
             for op in operations.items:
-                deposites.append({'currency': op.payment.currency, 'amount': op.payment.units,
-                                  'date': op.date.strftime('%Y-%m-%d')})
+                op_dtos.append({'currency': op.payment.currency,
+                                  'amount': op.payment.units, 'date': op.date.strftime('%Y-%m-%d')})
 
-        for dep in deposites:
-            print(dep['currency'], dep['amount'], dep['date'])
+            while operations.has_next:
+                request = get_request(cursor=operations.next_cursor)
+                operations = client.operations.get_operations_by_cursor(request)
+                for op in operations.items:
+                    op_dtos.append({'currency': op.payment.currency, 'amount': op.payment.units,
+                                      'date': op.date.strftime('%Y-%m-%d')})
+
+        for op in op_dtos:
+            print(op['currency'], op['amount'], op['date'], sep='\t')
 
 
-def get_portfolio_sum():
+def get_portfolios_sum():
     with Client(token) as client:
-        accounts = client.users.get_accounts()
-        account_id = accounts.accounts[0].id
-
-        portfolio = client.operations.get_portfolio(account_id=account_id)
-        return portfolio.total_amount_portfolio.units
+        accounts = get_acc_ids()
+        total = 0
+        for account_id in accounts:
+            portfolio = client.operations.get_portfolio(account_id=account_id)
+            total += portfolio.total_amount_portfolio.units
+        return total
 
 
 if __name__ == "__main__":
-    portfolio_sum = get_portfolio_sum()
+    print(get_acc_ids())
+    portfolio_sum = get_portfolios_sum()
     print(f"Текущая  рублёвая стоимость портфеля: {portfolio_sum:n} руб\n")
 
     print_deposites()
